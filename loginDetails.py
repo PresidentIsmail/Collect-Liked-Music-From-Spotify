e2751eaddfe3662edb48bccc912d1cfda1bbbe23
Ismail <shaikhnagismail@gmail.com>
2021-11-04 15:52:29 +0200

    rename youtube password variable

--- loginDetails.py
+++ loginDetails.py
@@ -2,7 +2,7 @@ spotify_musicGetting_username = "itisme.ismail@gmail.com"
 spotify_musicGetting_password = "9917$ismaiL"
 
 youtube_musicGetting_email = "imnobystander@gmail.com"
-youtube_musicGetting_email_password = "9917$Bystander"
+youtube_musicGetting_password = "9917$Bystander"
 
 youtube_musicSending_email = ""
 youtube_musicSending_password = ""
