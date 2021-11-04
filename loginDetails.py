c788c76e25960d18e2924df46d12cb4617e15f38
Ismail <shaikhnagismail@gmail.com>
2021-11-04 15:37:31 +0200

    add vaiables for new youtube account

--- loginDetails.py
+++ loginDetails.py
@@ -1,5 +1,8 @@
-username = "itisme.ismail@gmail.com"
-password = "9917$ismaiL"
+spotify_musicGetting_username = "itisme.ismail@gmail.com"
+spotify_musicGetting_password = "9917$ismaiL"
 
-email = "imnobystander@gmail.com"
-email_password = "9917$Bystander"
+youtube_musicGetting_email = "imnobystander@gmail.com"
+youtube_musicGetting_email_password = "9917$Bystander"
+
+youtube_musicSending_email = ""
+youtube_musicSending_email_password = ""
