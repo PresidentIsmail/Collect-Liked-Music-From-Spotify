52e753f853edf0cd594d07eef7772fb238c79329
Ismail <shaikhnagismail@gmail.com>
2021-11-17 17:34:16 +0200

    update musicSend email and password

--- loginDetails.py
+++ loginDetails.py
@@ -4,5 +4,5 @@ spotify_musicGetting_password = "9917$ismaiL"
 youtube_musicGetting_email = "imnobystander@gmail.com"
 youtube_musicGetting_password = "9917$Bystander"
 
-youtube_musicSending_email = "ireallyamsocrates@gmail.com"
-youtube_musicSending_password = "9917$Socrates"
+youtube_musicSending_email = "imnotpascal@gmail.com"
+youtube_musicSending_password = "9917$Pascal"
