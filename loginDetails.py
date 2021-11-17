c523e237c8909e9be7ba68be8052216e70972adc
Ismail <shaikhnagismail@gmail.com>
2021-11-17 13:16:52 +0200

    add email address and password

--- loginDetails.py
+++ loginDetails.py
@@ -4,5 +4,5 @@ spotify_musicGetting_password = "9917$ismaiL"
 youtube_musicGetting_email = "imnobystander@gmail.com"
 youtube_musicGetting_password = "9917$Bystander"
 
-youtube_musicSending_email = ""
-youtube_musicSending_password = ""
+youtube_musicSending_email = "ireallyamsocrates@gmail.com"
+youtube_musicSending_password = "9917$Socrates"
