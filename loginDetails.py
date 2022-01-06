00d3ba5a9898ee42d9299bcfd6ca13d83bdbcec4
Ismail <shaikhnagismail@gmail.com>
2022-01-06 19:17:18 +0200

    remove loginDetails from repo

--- loginDetails.py
+++
@@ -1,8 +0,0 @@
-spotify_musicGetting_username = "itisme.ismail@gmail.com"
-spotify_musicGetting_password = "9917$ismaiL"
-
-youtube_musicGetting_email = "imnotpascal@gmail.com"
-youtube_musicGetting_password = "9917$Pascal"
-
-youtube_musicSending_email = "imnotpascal@gmail.com"
-youtube_musicSending_password = "9917$Pascal"
